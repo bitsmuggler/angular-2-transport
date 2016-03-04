2f2e3ad86639ee87353ae698ec6175886be998c0
Patrick Roos <pa.roos@icloud.com>
2016-03-04 13:35:51 +0000

    Typescript Watcher, Browsersync & Live-Reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,53 +3,53 @@ var del = require('del');
 var typescript = require('gulp-typescript');
 var tscConfig = require('./tsconfig.json');
 var inject = require('gulp-inject');
-var gnf = require('gulp-npm-files');
 var watch = require('gulp-watch');
-var batch = require('gulp-batch');
-var connect = require('gulp-connect');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
   return del('dist/**/*');
 });
 
-
-// TypeScript compile
-gulp.task('compile', ['clean'], function () {  
-  return gulp
-    .src(['app/**/*.ts'])
-    .pipe(typescript(tscConfig.compilerOptions))
-    .pipe(gulp.dest('app'))
-    .pipe(connect.reload());
+gulp.task('compile-ts', function() {    
+    gulp.src(['app/**/*.ts'])
+        .pipe(typescript(tscConfig.compilerOptions))
+        .pipe(gulp.dest('app'));
 });
 
-
-// Copy dependencies to build/node_modules/ by 
-// value in './path/to/package.json' file 
-gulp.task('copy:libs', function() {
-  gulp
-    .src(gnf(null, 'package.json'), {base:'./'})
-    .pipe(gulp.dest('./dist'));
-});
- 
-gulp.task('copy:assets', ['copy:libs'], function() {
-  return gulp.src(['config.js', 'app/**/*', 'index.html', '!app/**/*.ts'], { base : './' })
-             .pipe(gulp.dest('dist'))
+gulp.task('server:watch', ['compile-ts'], function() {  
+    gulp.watch("**/*.ts", 
+        { cwd : './', read:false, debounceDelay: 50 }, ['compile-ts']);    
 });
 
-gulp.task('server:watch', function () {
-    watch('**/*.ts', batch(function (events, done) {
-        console.log('Typescript file was changed. Start transpiler...');
-        gulp.start('compile', done);
-    }));
+gulp.task('server:browsersync', function () {  
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        files: [
+            "**/*.js",
+            "**/*.css",
+            "**/*.html"
+        ],
+        watchOptions: {
+            debounceDelay: 100,
+            cwd: './'
+        },
+        logFileChanges: false
+    });
 });
 
-gulp.task('server:serve', function() {
-  connect.server({
-    livereload: true
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
   });
 });
 
-gulp.task('build', ['compile']);
+gulp.task('build', ['compile-ts']);
 gulp.task('default', ['build']);
-gulp.task('server', ['server:watch', 'server:serve']);
+gulp.task('server', ['server:watch', 'server:browsersync']);
